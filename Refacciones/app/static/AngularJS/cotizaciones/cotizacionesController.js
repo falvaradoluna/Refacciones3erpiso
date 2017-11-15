6fc4659f1350f9ea053b29969bf01eac38759594
destajador57 <destajador57@hotmail.com>
2017-11-15 11:06:10 -0600

    Flujo de modales para el alta de refacciones

--- Refacciones/app/static/AngularJS/cotizaciones/cotizacionesController.js
+++ Refacciones/app/static/AngularJS/cotizaciones/cotizacionesController.js
@@ -5,14 +5,52 @@ registrationModule.controller('cotizacionesController', function ($scope, $rootS
     $('[data-toggle="tooltip"]').tooltip();
 
     $scope.cotizaciones = [];
+    $scope.partesAgregadas = [];
 
     $scope.reiniciaBusqueda = function(){
         $scope.busquedaCotizacion = '';
     }
 
+    $scope.getMarcas = function(){
+        var data = [
+            {
+                id: 0,
+                nombre:'Selecciona ...'
+            },
+            {
+                id: 1,
+                nombre:'NISSAN'
+            },
+            {
+                id: 2,
+                nombre:'GM'
+            }
+        ]
+        $scope.marcas = data;
+        $scope.marcaSeleccionada = $scope.marcas[0];
+    };
+
+    $scope.getMarcas();
+
+    $scope.getDirecciones = function(){
+        var data = [
+            {
+                id: 1,
+                nombre:'Calle michoacan sur 28, cp 90980 San pablo del monte Tlaxcala.'
+            },
+            {
+                id: 2,
+                nombre:'Calle nahuatlacas 365, cp 04300 Ajusco del coyoacan.'
+            }
+        ]
+        $scope.direcciones = data;
+    };
+
+    $scope.getDirecciones();
+
     $scope.getCotizaciones = function () {
         
-        data = [{
+        var data = [{
             folio: 12345
         },
         { folio: 5234 },
@@ -25,19 +63,134 @@ registrationModule.controller('cotizacionesController', function ($scope, $rootS
 
     $scope.getCotizaciones();
 
-    // ------------------------------------------ AGREGA COTIZACIONES
+    // ------------------------------------------ AGREGA PARTE
     
     $scope.showAgregarCotizacion = function () {
         
         $scope.agregaCotizacion = {
             folio: ''
         };
-        $('#modalAgrega').modal('show');
+
+        $scope.refaccionBusquedaPorVIN = '';
+        $scope.refaccionBusqueda = '';
+        $scope.busquedaActual = []
+        $scope.partesAgregadas = [];
+        $scope.marcaSeleccionada = $scope.marcas[0];
+
+
+        $('#modalAgregaParte').modal('show');
     };
 
-    $scope.agregarCotizacion = function(){
+    $scope.clearQueryPorVIN = function(){
+        $scope.refaccionBusquedaPorVIN = '';
+    };
 
-        $scope.cotizaciones.push($scope.agregaCotizacion);
+    $scope.buscarRefaccionPorVIN = function(){
+        if($scope.refaccionBusquedaPorVIN && $scope.refaccionBusquedaPorVIN.length > 0){
+            angular.forEach($scope.marcas, function(marca, key) {
+                if(marca.nombre == $scope.refaccionBusquedaPorVIN){
+                    $scope.marcaSeleccionada = marca;
+                    console.log('si es igual la marca, por eso la va a setear');
+                }
+              });
+            console.log('Busca el VIN para obtener la marca');
+        }
+    };
+
+$scope.limpiarBusquedaParte = function(){
+    $scope.refaccionBusqueda = '';
+};
+
+$scope.buscarRefaccion = function(){
+    if($scope.refaccionBusqueda.length > 3){
+        $scope.busquedaActual = [
+            {
+                id:1,
+                PTS_IDPARTE:'876ASDF',
+                PTS_DESPARTE: 'foco para nissan',
+                PTS_PCOLISTA:800,
+                cantidad: 1
+            },
+            {
+                id:2,
+                PTS_IDPARTE:'345HKU',
+                PTS_DESPARTE: 'facia para nissan',
+                PTS_PCOLISTA:800,
+                cantidad:1
+            },
+            {
+                id:2,
+                PTS_IDPARTE:'RTU34',
+                PTS_DESPARTE: 'toldo para nissan',
+                PTS_PCOLISTA:800,
+                cantidad:1
+            },
+            {
+                id:3,
+                PTS_IDPARTE:'CVNX345',
+                PTS_DESPARTE: 'parabrisas para nissan',
+                PTS_PCOLISTA:800,
+                cantidad:1
+            },
+        ];
+    };
+};
+
+$scope.agregarParte = function(parteAgregada){
+
+    var agregado = false;
+    
+    if($scope.partesAgregadas.length > 0){
+        angular.forEach($scope.partesAgregadas, function(parte, key) {
+            if(parte.id == parteAgregada.id){
+                parte.cantidad += parteAgregada.cantidad;
+                agregado = true;
+            }
+          });
+    };
+
+    if(!agregado){
+        $scope.partesAgregadas.push(parteAgregada);
+    };
+
+    $scope.busquedaActual = []
+    $scope.refaccionBusqueda = '';
+};
+
+$scope.eliminarParte = function (idx){
+    $scope.partesAgregadas.splice(idx, 1);
+};
+
+$scope.agregarCotizacionParte = function(){
+    $('#modalAgregaDireccion').modal('show');
+    //modalModifica
+            //$scope.cotizaciones.push($scope.agregaCotizacion);
+        };
+
+    // ------------------------------------------ AGREGA DIRECCION
+
+    $scope.backCotizacionParte = function(){
+        $('#modalAgregaParte').modal('show');
+    }
+
+
+
+
+    $scope.agregarCotizacionConfirma = function (){
+        $('#modalAgregaConfirma').modal('show');
+    }
+
+    // ------------------------------------------ AGREGA CONFIRMA PEDIDO
+
+    $scope.backCotizacionDireccion = function(){
+        $('#modalAgregaDireccion').modal('show');
+    }
+
+
+
+
+    $scope.agregarCotizacion = function (){
+        
     }
 
     // ------------------------------------------ MODIFICA COTIZACIONES
