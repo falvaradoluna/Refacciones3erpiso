3c7265a1f252d314d9f5269660d25fa3a6047e1f
JesusSanYan <jjsanyan@gmail.com>
2017-11-29 17:38:01 -0600

    cargaArchivos
    Repositorio y controller

--- Refacciones/app/static/AngularJS/cargaArchivos/cargaArchivosController.js
+++ Refacciones/app/static/AngularJS/cargaArchivos/cargaArchivosController.js
@@ -1,16 +1,3 @@
-registrationModule.controller('configuradorGruposController', function ($route, $scope, $rootScope, $location, configuradorGruposRepository, userFactory, alertFactory) {
-    $scope.Grupos = [];
-    $scope.userData = {};
-
-    $scope.init = function () {
-        //redirecciona al login si no hay un usuario logeado
-        var userData = userFactory.getUserData();
-        if(userData==null||userData==undefined)
-            location.href = '/';
-    
-        $scope.userData = userFactory.getUserData();
-
-    };
-
+registrationModule.controller('cargaArchivosController', function ($route, $scope, $rootScope, $location, cargaArchivosRepository, userFactory, alertFactory) {
 
 });
---
+++ Refacciones/app/static/AngularJS/cargaArchivos/cargaArchivosRepository.js
@@ -0,0 +1,9 @@
+var cargaArchivosURL = global_settings.urlCORS + 'api/CargaArchivos';
+
+registrationModule.factory('cargaArchivosRepository', function ($http) {
+    return{
+
+    };
+
+});
+    
