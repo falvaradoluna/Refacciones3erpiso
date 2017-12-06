4a9a97181ca6a0b6d223cb939ed0b35a6cfa5143
Alcabelu <alcabelu@gmail.com>
2017-12-06 09:40:46 -0600

    Se modifica el puerto de la aplicacion a 1966, ya que en el servidor 9 el puerto 4500 ya se encuentra en uso

--- Refacciones/app/static/confront.js
+++ Refacciones/app/static/confront.js
@@ -1,4 +1,4 @@
 var global_settings = {
-    urlCORS: 'http://localhost:4500/'
-    //urlCORS: 'http://192.168.20.9:4500/' 
+    urlCORS: 'http://localhost:1966/'
+    //urlCORS: 'http://192.168.20.9:1966/' 
 }
